a2f561c2009e179e697a21c85dbbe75ae9c3266b
JHKoder <jeonghun.kang.dev@gmail.com>
2024-02-28 09:11:21 +0900

    Initial commit

---
+++ src/main/java/github/jhkoder/pickupBigxel_server/PickupBigxelServerApplication.java
@@ -0,0 +1,13 @@
+package github.jhkoder.pickupBigxel_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupBigxelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickupBigxelServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/jhkoder/pickupBigxel_server/PickupBigxelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.jhkoder.pickupBigxel_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupBigxelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
