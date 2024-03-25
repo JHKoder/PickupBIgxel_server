888a72026f8cd2761aeae7866a2d78dc3b1b98ac
JHKoder <jeonghun.kang.dev@gmail.com>
2024-03-25 22:33:57 +0900

    :memo: 서브 모듈 가저오는 데이터

--- src/main/java/github/jhkoder/rest/EnvironmentModuleData.java
+++ src/main/java/github/jhkoder/rest/EnvironmentModuleData.java
@@ -5,19 +5,31 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @ConfigurationProperties(prefix = "environment")
 @PropertySource(value = "classpath:environment-modules.yml", factory = EnvironmentModuleConfigFactory.class)
 public class EnvironmentModuleData {
-    private List<String> modules;
+    private List<EnvironmentModuleDataRequest> modules;
 
-    public List<String> getModules() {
+    public List<EnvironmentModuleDataRequest> getModules() {
         return modules;
     }
 
-    public void setModules(List<String> modules) {
+    public void setModules(Map<String, String> modules) {
+        this.modules = new ArrayList<>();
+        for (Map.Entry<String, String> entry : modules.entrySet()) {
+            String moduleName = entry.getKey();
+            String jarName = entry.getValue();
+            EnvironmentModuleDataRequest request = new EnvironmentModuleDataRequest(moduleName, jarName);
+            this.modules.add(request);
+        }
+    }
+
+    public EnvironmentModuleData(List<EnvironmentModuleDataRequest> modules) {
         this.modules = modules;
     }
 }
